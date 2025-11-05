a77cf42e0902d95666c9afc2c852dfbf6713b9be
Cristian Pasat <cristian.pasat@student.pxl.be>
2025-11-05 19:24:46 +0100

    Project Created

---
+++ mergeTest/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace mergeTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+                            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, World!");
+
+
+
+
+        }
+    }
+}
